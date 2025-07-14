03c616dcd25de7c26819fe205f9b32afc7009106
VincT89 <vincenzotito@icloud.com>
2025-07-14 19:00:21 +0200

    sottoclasse elettrica

--- Javascript02/script.js
+++ Javascript02/script.js
@@ -13,7 +13,7 @@ class Automobile {
   }
 
   descrizione() {
-    return `Marca: ${this.marca}, Modello: ${this.modello}, Anno: ${this.anno}, Chilometraggio attuale: ${this.chilometraggio} km.`;
+    return `Marca: ${this.marca}, Modello: ${this.modello}, Anno: ${this.anno}, Chilometraggio attuale: ${this.chilometraggio} km`;
   }
  
   // AGGIUNGI METODI & PROPRIETA'
@@ -21,7 +21,7 @@ class Automobile {
     if (km > 0) {
       this.chilometraggio += km;
     } else {
-      console.alert('Inserisci un valore positivo.')
+      console.error('Inserisci un valore positivo.')
     }
   }
 
@@ -37,4 +37,32 @@ console.log(auto);
 auto.aggiungiChilometri(80000);
 console.log(auto.mostraChilometraggio());
 
+
+// SOTTOCLASSE ELETTRICA
+class Elettrica extends Automobile {
+  autonomia
+  
+  constructor(marca, modello, anno, chilometraggio, autonomia) {
+    super(marca, modello, anno, chilometraggio);
+    this.autonomia = autonomia
+  }
+
+  descrizione() {
+    return `${super.descrizione()}, Autonomia batteria: ${this.autonomia} km`
+  }
+
+  ricarica(km) {
+    if (km > 0) {
+      this.autonomia += km;
+    } else {
+      console.error('Inserisci un valore positivo per la ricarica.')
+    }
+  }
+}
+
+let autoElettrica = new Elettrica('Tesla', 'Model 3', 2022, 80000, 400)
+console.log(autoElettrica.descrizione());
+
+autoElettrica.ricarica(100);
+console.log(autoElettrica.descrizione());
 
