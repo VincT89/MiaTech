dadad273f349088311a8745fcc6a9a3932f5f168
VincT89 <vincenzotito@icloud.com>
2025-07-17 18:00:02 +0200

    aggiungi km

--- Javascript02/script.js
+++ Javascript02/script.js
@@ -1,120 +1,128 @@
 // AUTOMOBILE
 class Automobile {
-  marca
-  modello 
-  anno 
-  chilometraggio = 0
-  #contatoreChiamate = 0;
+	marca;
+	modello;
+	anno;
+	chilometraggio = 0;
+	#contatoreChiamate = 0;
 
-  constructor(marca, modello, anno, chilometraggio) {
-    this.marca = marca;
-    this.modello = modello;
-    this.anno = anno;
-    this.chilometraggio = chilometraggio;
-  }
+	constructor(marca, modello, anno, chilometraggio) {
+		this.marca = marca;
+		this.modello = modello;
+		this.anno = anno;
+		this.chilometraggio = chilometraggio;
+	}
 
-  descrizione() {
-    const avviso = this._controllaChilometri();
-    this.#incrementaContatore();
-    return `Marca: ${this.marca}, Modello: ${this.modello}, Anno: ${this.anno}, Chilometraggio attuale: ${this.chilometraggio} km  ${avviso}, Chiamate ricevute: ${this.#contatoreChiamate}`
+	descrizione() {
+		const avviso = this._controllaChilometri();
+		return `Marca: ${this.marca}, Modello: ${this.modello}, Anno: ${
+			this.anno
+		}, Chilometraggio attuale: ${
+			this.chilometraggio
+		} km  ${avviso}`;
+	}
+
+	// AGGIUNGI METODI & PROPRIETA'
+	aggiungiChilometri(km) {
+		if (km > 0) {
+			this.chilometraggio += km;
+		} else {
+			console.error("Inserisci un valore positivo.");
+		}
+	}
+
+	mostraChilometraggio() {
+		return this.chilometraggio;
+	}
+
+	// METODO PRIVATO - ETA'
+
+	#calcolaEtà() {
+		const annoCorrente = new Date().getFullYear();
+		return annoCorrente - this.anno;
+	}
+
+	mostraEtà() {
+		const eta = this.#calcolaEtà();
+		return `Età del veicolo: ${eta} anni`;
+	}
+
+	// METODO PROTETTO - AVVISO
+	_controllaChilometri() {
+		const soglia = 100000;
+		if (this.chilometraggio > soglia) {
+			return `Attenzione: chilometraggio elevato: ${this.chilometraggio} km`;
+		}
+		return `Acquistabile`;
+	}
+
+	// METODO STATICO - CONFRONTO CHILOMETRAGGIO
+	static confrontaChilometraggio(auto1, auto2) {
+		if (auto1.chilometraggio > auto2.chilometraggio) {
+			return `${auto1.marca} ${auto1.modello} ha un chilometraggio maggiore (${auto1.chilometraggio} km)`;
+		} else if (auto2.chilometraggio > auto1.chilometraggio) {
+			return `${auto2.marca} ${auto2.modello} ha un chilometraggio maggiore (${auto2.chilometraggio} km)`;
+		} else {
+			return `Entrambe le auto hanno lo stesso chilometraggio (${auto1.chilometraggio} km)`;
+		}
+	}
+
+	// METODO PRIVATO - INCREMENTA CONTATORE
+	#incrementaContatore() {
+		this.#contatoreChiamate++;
   }
- 
-  // AGGIUNGI METODI & PROPRIETA'
+  
+  // METODO PUBBLICO - AGGIUNGI CHILOMETRI
   aggiungiChilometri(km) {
-    if (km > 0) {
-      this.chilometraggio += km;
-    } else {
-      console.error('Inserisci un valore positivo.')
+    this.#incrementaContatore();
     }
   }
 
-  mostraChilometraggio() {
-    return this.chilometraggio;
-  }
 
-  // METODO PRIVATO - ETA'
-
-  #calcolaEtà() {
-    const annoCorrente = new Date().getFullYear();
-    return annoCorrente - this.anno;
-  }
-
-  mostraEtà() {
-    const eta = this.#calcolaEtà();
-    return `Età del veicolo: ${eta} anni`
-  }
-
-  // METODO PROTETTO - AVVISO
-  _controllaChilometri() {
-    const soglia = 100000;
-    if (this.chilometraggio > soglia) {
-      return `Attenzione: chilometraggio elevato: ${this.chilometraggio} km`;
-    }
-    return `Acquistabile`;
-  }
-
-  // METODO STATICO - CONFRONTO CHILOMETRAGGIO
-  static confrontaChilometraggio(auto1, auto2) {
-    if (auto1.chilometraggio > auto2.chilometraggio) {
-      return `${auto1.marca} ${auto1.modello} ha un chilometraggio maggiore (${auto1.chilometraggio} km)`;
-    } else if (auto2.chilometraggio > auto1.chilometraggio) {
-      return `${auto2.marca} ${auto2.modello} ha un chilometraggio maggiore (${auto2.chilometraggio} km)`;
-    } else {
-      return `Entrambe le auto hanno lo stesso chilometraggio (${auto1.chilometraggio} km)`
-    }
-  }
-
-  // METODO PRIVATO - INCREMENTA CONTATORE
-  #incrementaContatore() {
-     this.#contatoreChiamate++;
-  }
-
-}
-
-let auto = new Automobile('VW', 'Golf', 2020, 120000);
+let auto = new Automobile("VW", "Golf", 2020, 120000);
 console.log(auto);
 
 auto.aggiungiChilometri(80000);
 console.log(auto.mostraChilometraggio());
 
-
 // SOTTOCLASSE ELETTRICA
 class Elettrica extends Automobile {
-  autonomia
-  
-  constructor(marca, modello, anno, chilometraggio, autonomia) {
-    super(marca, modello, anno, chilometraggio);
-    this.autonomia = autonomia
-  }
+	autonomia;
 
-  descrizione() {
-    const avviso = this._controllaChilometri();
-    return `${super.descrizione()}, Autonomia batteria: ${this.autonomia} km, ${avviso}`;
-  }
+	constructor(marca, modello, anno, chilometraggio, autonomia) {
+		super(marca, modello, anno, chilometraggio);
+		this.autonomia = autonomia;
+	}
 
-  ricarica(km) {
-    if (km > 0) {
-      this.autonomia += km;
-    } else {
-      console.error('Inserisci un valore positivo per la ricarica.')
-    }
-  }
+	descrizione() {
+		const avviso = this._controllaChilometri();
+		return `${super.descrizione()}, Autonomia batteria: ${
+			this.autonomia
+		} km, ${avviso}`;
+	}
+
+	ricarica(km) {
+		if (km > 0) {
+			this.autonomia += km;
+		} else {
+			console.error("Inserisci un valore positivo per la ricarica.");
+		}
+	}
 }
 
-let autoElettrica = new Elettrica('Tesla', 'Model 3', 2022, 1000000, 400)
+let autoElettrica = new Elettrica("Tesla", "Model 3", 2022, 1000000, 400);
 console.log(autoElettrica.descrizione());
 
 autoElettrica.ricarica(100);
 console.log(autoElettrica.descrizione());
 
-
 // SALUTA
 Automobile.prototype.saluta = function () {
-  return `Ciao! Sono una ${this.marca} ${this.modello}.`;
+	return `Ciao! Sono una ${this.marca} ${this.modello}.`;
 };
 
-let auto2 = new Automobile('Fiat', 'Panda', 2015, 180000);
-let autoElettrica2 = new Elettrica('Tesla', 'Model S', 2023, 10000, 500);
+let auto2 = new Automobile("Fiat", "Panda", 2015, 180000);
+let autoElettrica2 = new Elettrica("Tesla", "Model S", 2023, 10000, 500);
 
 console.log(auto2.saluta());
 console.log(autoElettrica2.saluta());
@@ -125,4 +133,3 @@ console.log(autoElettrica2.descrizione());
 
 let confronto = Automobile.confrontaChilometraggio(auto, autoElettrica);
 console.log(confronto);
-
