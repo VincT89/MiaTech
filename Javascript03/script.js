1248ba90d610110918a07e45e5f9583c7b41afa8
VincT89 <vincenzotito@icloud.com>
2025-07-22 18:30:27 +0200

    saltare elementi durante il destructuring

--- Javascript03/script.js
+++ Javascript03/script.js
@@ -1,6 +1,9 @@
 // ESTRAZIONE ELEMENTI BASE
 let num = [1, 2, 3, 4, 5];
 let [primo, secondo] = num;
-
 console.log(primo, secondo);
 
+// SALTARE ELEMENTI DURANTE IL DESTRUCTURING
+const numeri = [10, 20, 30, 40, 50];
+const [primoNum, , terzoNum] = numeri;
+console.log(primoNum, terzoNum);
