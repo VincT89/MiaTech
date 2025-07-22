cc709630780796ef5439f9926072854670cf1649
VincT89 <vincenzotito@icloud.com>
2025-07-22 18:31:25 +0200

    destructuring con valori predefiniti

--- Javascript03/script.js
+++ Javascript03/script.js
@@ -7,3 +7,8 @@ console.log(primo, secondo);
 const numeri = [10, 20, 30, 40, 50];
 const [primoNum, , terzoNum] = numeri;
 console.log(primoNum, terzoNum);
+
+// DESTRUCTURING CON VALORI PREDEFINITI
+let nume = [1, 2];
+let [a = 0, b = 0, c = 0, d = 0, e = 0] = nume;
+console.log(a, b, c, d, e);
