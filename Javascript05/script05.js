fd7c03c8dd70f6ad6dff41c9c9108be6440b8876
VincT89 <vincenzotito@icloud.com>
2025-08-23 17:37:58 +0200

    utilizzare API che supporta CORS

--- Javascript05/script05.js
+++ Javascript05/script05.js
@@ -7,3 +7,12 @@ fetch(`${proxy}/?url=${encodeURIComponent(api)}`)
   .then(data => console.log(data))
   .catch(error => console.error("Error:", error));
 
+// UTILIZZARE UN API CHE SUPPORTA CORS
+const corsApi = "https://api.publicapis.org/entries";
+
+fetch(corsApi)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
+
+  
