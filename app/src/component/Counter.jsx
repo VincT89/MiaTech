75ed2c4b39e9ed738219f98d92000bcd879b74a4
VincT89 <vincenzotito@icloud.com>
2025-09-10 18:00:44 +0200

    contatore semplice - parte 2

--- app/src/component/Counter.jsx
+++ app/src/component/Counter.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Counter = () => {
+const[count, setCount] = useState(0);
+
   return (
     <div className='flex items-center justify-center mt-100'>
-      <p className='text-2xl font-bold'>Counter</p>
+      <p className='text-2xl font-bold'>Counter {count}</p>
     </div>
   )
 }
