e412d79fc72292a0f89175ff3e0ff0655e1e0acb
VincT89 <vincenzotito@icloud.com>
2025-09-10 18:36:42 +0200

    submit

--- app/src/component/LoginForm.jsx
+++ app/src/component/LoginForm.jsx
@@ -3,10 +3,15 @@ import { useState } from "react";
 
 export const LoginForm = () => {
 	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    alert(`Username: ${username}\nPassword: ${password}`);
+  }
 
 	return (
-		<form className="flex flex-col gap-4 items-center justify-center mt-100">
+		<form onSubmit={handlerSubmit} className="flex flex-col gap-4 items-center justify-center mt-100">
 			<input
 				type="text"
 				value={username}
@@ -19,8 +24,9 @@ export const LoginForm = () => {
 				value={password}
 				onChange={(e) => setPassword(e.target.value)}
 				placeholder="Password"
-				className="mt-4 p-2 border"
-			/>
+        className="mt-4 p-2 border"
+      />
+      <button type="submit" className="rounded mt-5 w-">Submit</button>
 		</form>
 	);
 };
