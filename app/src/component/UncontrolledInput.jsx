37f0429489053527a841f0271c6bcd62ee3be211
VincT89 <vincenzotito@icloud.com>
2025-09-10 18:43:35 +0200

    usare ref per accedere al valore input 2

--- app/src/component/UncontrolledInput.jsx
+++ app/src/component/UncontrolledInput.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useRef } from 'react';
 
 export const UncontrolledInput = () => {
+  const inputRef = useRef(null);
   return (
     <div className='flex items-center justify-center mt-100'>
-      <input type="text" placeholder='Scrivi qui...' className='border p-2' />
+      <input type="text" placeholder='Scrivi qui...' className='border p-2' ref={inputRef} />
     </div>
   )
 }
