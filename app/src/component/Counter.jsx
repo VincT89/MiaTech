1d63ae467179532db5dbecf5fc449ccce6336e5b
VincT89 <vincenzotito@icloud.com>
2025-09-10 18:12:11 +0200

    decrementa contatore

--- app/src/component/Counter.jsx
+++ app/src/component/Counter.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export const Counter = () => {
-const[count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className='flex items-center justify-center mt-100'>
-      <div>
-        <p className='text-2xl font-bold'>Counter {count}</p>
-        <button onClick={() => setCount(count + 1)} className='mt-4 p-2 text-white'>
-          Incrementa
-        </button>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="flex items-center justify-center mt-100">
+			<div>
+				<p className="text-2xl font-bold text-center">Counter {count}</p>
+				<div className="flex gap-4">
+					<button onClick={() => setCount(count + 1)} className="mt-4 p-2 text-white">
+						Incrementa
+          </button>
+          <button onClick={() => setCount(count - 1)} className="mt-4 p-2 text-white">
+						Decrementa
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
