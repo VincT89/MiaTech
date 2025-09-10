c2400b7701f241095cf6aa0daac2839c354a36ae
VincT89 <vincenzotito@icloud.com>
2025-09-10 18:57:21 +0200

    rendere una lista di elementi

---
+++ app/src/component/ItemList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ItemList = ({items}) => {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  )
+}
