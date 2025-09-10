ff0a2bceed13e8e9b2c64c559e60cb89d696d16e
VincT89 <vincenzotito@icloud.com>
2025-09-10 18:52:45 +0200

    usare ref per accedere valore input 3

--- app/src/component/UncontrolledInput.jsx
+++ app/src/component/UncontrolledInput.jsx
@@ -3,9 +3,15 @@ import { useRef } from 'react';
 
 export const UncontrolledInput = () => {
   const inputRef = useRef(null);
+  const handleClick = () => {
+    alert(`Valore corrente: ${inputRef.current.value}`)
+  }
   return (
     <div className='flex items-center justify-center mt-100'>
-      <input type="text" placeholder='Scrivi qui...' className='border p-2' ref={inputRef} />
+      <input type="password" placeholder='Scrivi qui...' className='border p-2' ref={inputRef}  />
+      <button onClick={handleClick} className="ml-4 p-2 text-white">
+						Mostra
+					</button>
     </div>
   )
 }
