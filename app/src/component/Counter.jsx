798f7710d1546056c2c9af00b876887cd5e1bcf3
VincT89 <vincenzotito@icloud.com>
2025-09-10 18:04:21 +0200

    incrementa contatore

--- app/src/component/Counter.jsx
+++ app/src/component/Counter.jsx
@@ -5,7 +5,12 @@ const[count, setCount] = useState(0);
 
   return (
     <div className='flex items-center justify-center mt-100'>
-      <p className='text-2xl font-bold'>Counter {count}</p>
+      <div>
+        <p className='text-2xl font-bold'>Counter {count}</p>
+        <button onClick={() => setCount(count + 1)} className='mt-4 p-2 text-white'>
+          Incrementa
+        </button>
+      </div>
     </div>
   )
 }
