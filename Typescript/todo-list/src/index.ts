aebd81abe762afd448fa4a35bbd742a797351523
VincT89 <vincenzotito@icloud.com>
2025-10-27 17:07:49 +0100

    funzione per aggiungere un todo

--- Typescript/todo-list/src/index.ts
+++ Typescript/todo-list/src/index.ts
@@ -1,3 +1,16 @@
 import { Todo } from './types';
 
 const todos: Todo[] = [];
+
+// Funzione per aggiungere un todo
+function addTodo(title: string): void {
+  const newTodo: Todo = {
+    id: todos.length + 1,
+    title,
+    completed: false,
+  };
+  todos.push(newTodo);
+}
+
+
+
