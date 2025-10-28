5aa717f1c528a6ff2ec42678cbe8d05bff689535
VincT89 <vincenzotito@icloud.com>
2025-10-28 09:27:45 +0100

    metodo addTodo

--- Typescript/todo-list/src/User.ts
+++ Typescript/todo-list/src/User.ts
@@ -1,13 +1,27 @@
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 class User {
   id: number;
   name: string;
   email: string | undefined;
+  todos: Todo[];
 
   constructor(id: number, name: string, email?: string) {
     this.id = id;
     this.name = name;
     this.email = email;
+    this.todos = [];
+  }
+
+  // Metodo per aggiungere un todo all'utente
+  addTodo(newTodo: Todo): void {
+    this.todos.push(newTodo);
   }
 };
 
-export { User };
+export { User, Todo };
